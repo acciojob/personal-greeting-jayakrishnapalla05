c92a92d0a8d722abad187403428fca6ec2b22ca6
jayakrishnapalla05 <jayakrishnapalla05@gmail.com>
2024-04-20 19:54:32 +0530

    submitting code-Sat Apr 20 2024 19:54:29 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,19 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [text,setText]=useState("");
+  const copyText=(e)=>{
+    setText(e.target.value);
+  }
   return (
     <div>
         {/* Do not remove the main div */}
+        <label>Enter your name:</label>
+        <input type="text" onClick={copyText} />
+          <p>Hello {text}!</p>
+
     </div>
   )
 }
